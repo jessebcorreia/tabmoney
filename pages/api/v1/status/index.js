ab9e1ce3925632fd314aceddb800aededc9c18a3
= <jesse.bcorreia@gmail.com>
2024-07-26 17:26:39 -0300

    atualização

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,26 @@
 import database from "/infra/database.js";
 
-async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  res.status(200).json({ chave: "valor" });
+async function status(request, response) {
+  const updatedAt = new Date().toISOString();
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: 0,
+        version: databaseVersionValue,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,31 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+  const statusCode = response.status;
+
+  const {
+    updated_at,
+    dependencies: {
+      database: { max_connections, opened_connections, version },
+    },
+  } = await response.json();
+
+  const parsedUpdatedAt = new Date(updated_at).toISOString();
+  const databaseVersion = "16.0";
+
+  expect(statusCode).toBe(200);
+  expect(parsedUpdatedAt).toEqual(updated_at);
+  expect(version).toEqual(databaseVersion);
+  expect(max_connections).toEqual(100);
+  expect(opened_connections).toEqual(1);
+
+  /*  console.log(
+    "updated_at: " +
+      updated_at +
+      "\nmax_connections: " +
+      max_connections +
+      "\nopened_connections: " +
+      opened_connections +
+      "\nversion: " +
+      version,
+  ); */
 });
