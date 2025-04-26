065320575f1416c75a6e392975089bdb7ab1ce39
Jessé Bruno Correia <jesse.bcorreia@gmail.com>
2025-04-26 00:27:00 -0300

    fix database ssl connection

--- infra/database.js
+++ infra/database.js
@@ -7,7 +7,10 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
+    ssl:
+      process.env.NODE_ENV === "development"
+        ? false
+        : { rejectUnauthorized: false },
   });
 
   console.log("Credenciais do Postgres: ", {
